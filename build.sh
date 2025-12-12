00749d4facb4d90936352c78799aff3c0dfafd45
3A20 <3a20@3A20.local>
2025-12-12 12:03:13 +0900

    Prepare for deployment:settings,requiremwnts, build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 
-if [[ -n "$DJANGO_SUPERUSER_USERNAME"]];then
+if [[ -n "$DJANGO_SUPERUSER_USERNAME"]]; then
     echo "Creating superuser..."
 
     python manage.py createsuperuser --noinput||echo "Superuser already exists."
